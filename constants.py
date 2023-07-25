75f8e6c3413f1cc2d7c7ad955abb94a49ded7836
Sylvester <sylvester.broich@maibornwolff.de>
2023-07-25 15:52:16 +0200

    first commit

---
+++ constants.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = "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"
